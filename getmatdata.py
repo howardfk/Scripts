299b6d0157f9c96e1dd3b292e2753e75135e3537
howard <howardfk@gmail.com>
2015-04-18 21:43:43 -0400

    Fixed double downloding of files due to pulling the file name twice from the html

--- getmatdata.py
+++ getmatdata.py
@@ -7,13 +7,13 @@ import timeit
 import signal
 
 def pullfiles():
-   start = timeit.default_timer()
    filenames = []
    savedir = "/Volumes/JIMSDISK/Data/"
 
    total = 0
    count = 0
    countpass = 0
+   testfind = 'SP140605223002_000.mat'
    #connect to a URL
    website = urllib2.urlopen("http://vlf-alexandria.stanford.edu/public_web_junk/southpole/2014/")
 
@@ -22,14 +22,14 @@ def pullfiles():
 
    #use re.findall to get all the links end with .mat
    filenames = re.findall('SP.*?\.mat', html)
-   total = len(filenames)
+   total = len(filenames)/2
    list_saved = os.listdir(savedir)
-   print list_saved[0], list_saved[1]
 
    for files in list_saved:
       if files.endswith(".mat"):
          #removing the files on the disk form the list of downloads
          try:
+            filenames.remove(files)
             filenames.remove(files)
             count += 1
          except ValueError:
@@ -40,18 +40,15 @@ def pullfiles():
    print "counted number of removes", count
    print "number files not removed:", len(filenames)
 
-#saves the file names into an array of html link
+   #saves the file names into an array of html link
    links=len(filenames)*[0]
    for j in range(len(filenames)): #len(filenames)):
       links[j] = 'http://vlf-alexandria.stanford.edu/public_web_junk/southpole/2014/'+filenames[j]
 
-
-   stop = timeit.default_timer()
-   print stop - start
-
    for i in range(len(links)):
+      pass
       print filenames[i]
-      #os.system("curl -o "+ filenames[i] + " " + str(links[i]))
+      os.system("curl -o "+ filenames[i] + " " + str(links[i]))
 
    print "links downloaded:",len(links)
 
